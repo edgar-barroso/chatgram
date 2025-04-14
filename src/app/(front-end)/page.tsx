9008105aad0475248b889a17d0db38a9ecdfc815
Jose Edgar Barroso Neto <barrosonetojose@gmail.com>
2025-04-14 02:07:41 -0300

    initial commit

--- src/app/(front-end)/page.tsx
+++ src/app/(front-end)/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Loading from "./_components/Loading";
 
 export default function Home() {
   const router = useRouter();
@@ -9,5 +10,5 @@ export default function Home() {
     router.push("/profile");
   }, [router]);
 
-  return null;
+  return <Loading />;
 }
