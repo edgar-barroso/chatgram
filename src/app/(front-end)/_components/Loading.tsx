f967cf0158e4eb290f000730c96f33b02e41b445
Jose Edgar Barroso Neto <barrosonetojose@gmail.com>
2025-04-14 02:04:12 -0300

    initial commit

--- src/app/(front-end)/_components/Loading.tsx
+++ src/app/(front-end)/_components/Loading.tsx
@@ -1,9 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex-1 flex">
     <div className="flex items-center justify-center h-full bg-foreground flex-1">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-ice-blue"></div>
-    </div>
     </div>
   );
 }
